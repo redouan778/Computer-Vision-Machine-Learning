917f3ea468d85be30061639f48a9ff747b29ce96
Redouan <redouan778@gmail.com>
2022-05-05 23:37:08 +0200

    opdracht 1.3

--- Source.cpp
+++ Source.cpp
@@ -10,19 +10,21 @@ using namespace std;
 /////////////////  Images  //////////////////////
 
 void main() {
-    printf("miauwww");
+	// Blank Image 
+	Mat img(512, 512, CV_8UC3, Scalar(255, 255, 255));
 
-    string path = "Resources/bert ernie.jpg";
-	Mat img = imread(path);
-	Mat imgResize, imgCrop;
+	rectangle(img, Point(100, 200), Point(410, 310), Scalar(0, 0, 255), FILLED);
 
-	resize(img, imgResize, Size(), 0.5, 0.5);
+	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), FILLED);
+	
+	line(img, Point(130, 296), Point(382, 296), Scalar(255, 255, 255), 2);
+	line(img, Point(130, 216), Point(382, 216), Scalar(255, 255, 255), 2);
+	
+	line(img, Point(120, 296), Point(120, 216), Scalar(255, 255, 255), 2);
+	line(img, Point(392, 296), Point(392, 216), Scalar(255, 255, 255), 2);
 
-	Rect roi(40, 100, 100, 30);
-	imgCrop = img(roi);
+	putText(img, "Redouan's image!!", Point(137, 262), FONT_HERSHEY_DUPLEX, 0.75, Scalar(255, 0, 0), 2);
 
 	imshow("Image", img);
-	imshow("Image Resize", imgResize);
-	imshow("Image Crop", imgCrop);
 	waitKey(0);
 }
