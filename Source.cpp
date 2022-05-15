42d19eb462153120c8a15745dfb3a4fc1f71161f
Redouan <redouan778@gmail.com>
2022-05-15 15:56:50 +0200

    opdracht 2.1

--- Source.cpp
+++ Source.cpp
@@ -10,21 +10,18 @@ using namespace std;
 /////////////////  Images  //////////////////////
 
 void main() {
-	// Blank Image 
-	Mat img(512, 512, CV_8UC3, Scalar(255, 255, 255));
 
-	rectangle(img, Point(100, 200), Point(410, 310), Scalar(0, 0, 255), FILLED);
+	string path = "Resources/cards.jpg";
+	Mat img = imread(path);
+	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
 
-	rectangle(img, Point(130, 226), Point(382, 286), Scalar(255, 255, 255), FILLED);
-	
-	line(img, Point(130, 296), Point(382, 296), Scalar(255, 255, 255), 2);
-	line(img, Point(130, 216), Point(382, 216), Scalar(255, 255, 255), 2);
-	
-	line(img, Point(120, 296), Point(120, 216), Scalar(255, 255, 255), 2);
-	line(img, Point(392, 296), Point(392, 216), Scalar(255, 255, 255), 2);
+	cvtColor(img, imgGray, COLOR_BGR2GRAY);
+	GaussianBlur(imgGray, imgBlur, Size(7, 7), 5, 0);
 
-	putText(img, "Redouan's image!!", Point(137, 262), FONT_HERSHEY_DUPLEX, 0.75, Scalar(255, 0, 0), 2);
 
 	imshow("Image", img);
+	imshow("Image Gray", imgGray);
+	imshow("Image Blur", imgBlur);
+
 	waitKey(0);
 }
